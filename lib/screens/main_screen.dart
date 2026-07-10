f9da2666fe435074efe5200795ab40d48be323a5
Asim Farooq <asim@xconn.io>
2026-07-10 14:01:32 +0500

    fix: add new branch to CI trigger + bottom nav shell + storage OpenAI key

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_screen.dart';
+import 'history_screen.dart';
+import 'settings_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  static const _pages = <Widget>[
+    HomeScreen(),
+    HistoryScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF141414),
+          border: Border(
+            top: BorderSide(
+              color: const Color(0xFF00E5FF).withOpacity(0.15),
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (i) => setState(() => _currentIndex = i),
+          backgroundColor: const Color(0xFF141414),
+          selectedItemColor: const Color(0xFF00E5FF),
+          unselectedItemColor: Colors.white38,
+          selectedLabelStyle: GoogleFonts.poppins(
+              fontSize: 11, fontWeight: FontWeight.w600),
+          unselectedLabelStyle: GoogleFonts.poppins(fontSize: 11),
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.electric_bolt_outlined),
+              activeIcon: Icon(Icons.electric_bolt),
+              label: 'Edit',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_outlined),
+              activeIcon: Icon(Icons.history),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              activeIcon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
